f072b8d23fe725535acad7a591a4084b2d39fe57
don-garu <hwangji9637@gmail.com>
2023-08-23 10:11:53 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useReducer } from 'react';
+import './style.css';
 
 export default function App() {
+  function countReducer(oldCount, action) {
+    if (action === 'UP') {
+      return oldCount + 1;
+    } else if (action === 'DOWN') {
+      return oldCount - 1;
+    } else if (action === 'RESET') {
+      return 0;
+    }
+  }
+
+  const [count, countDispatch] = useReducer(countReducer, 0);
+  function down() {
+    countDispatch('DOWN');
+  }
+  function reset() {
+    countDispatch('RESET');
+  }
+  function up() {
+    countDispatch('UP');
+  }
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <input type="button" value="-" onclick={down} />
+      <input type="button" value="0" onclick={reset} />
+      <input type="button" value="+" onclick={up} />
+      <span>{count}</span>
     </div>
   );
 }
